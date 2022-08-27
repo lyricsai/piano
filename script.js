280dd05c5c9c6d17fb67fc24208bba7afd1a52f5
Anastasiya Litvinenka <loffy@rambler.ru>
2022-08-27 18:34:45 +0300

    fix: mouse event

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const piano = () => {
     const keys = [...document.querySelectorAll('.key')];
 
     const playSound = (e) => {
-        if(e.pointerType === 'touch' || e.pointerType === 'mouse') e.keyCode = e.target.getAttribute('data-key');
+        if(e.pointerType === 'click' || e.pointerType === 'mouse') e.keyCode = e.target.getAttribute('data-key');
+        e.keyCode = e.target.getAttribute('data-key');
+
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
@@ -26,10 +28,8 @@ const piano = () => {
     };
 
     document.addEventListener('keyup', playSound, false);
-    keys.forEach(key => key.addEventListener('click', playSound));
-    keys.forEach(key => key.addEventListener('touchend', playSound));
+    keys.forEach(key => key.addEventListener('mouseup', playSound));
 
-    document.addEventListener('dblclick', (e) => e.preventDefault());
 };
 
 piano();
